108e340055640e6784229cea3832546480461273
Pedro Soares <pedroneto.ps180@gmail.com>
2020-08-11 09:20:04 -0300

    picpay

---
+++ src/components/PayButton/styles.js
@@ -0,0 +1,18 @@
+//import React from 'react'
+//import styled from 'styled-components'
+import styled from 'styled-components/native';
+
+import{LinearGradient} from 'expo-linear-gradient';
+
+export const Button = styled()`
+    width:60px;
+    height:60px;
+    border-radius:30px;
+    align-items:center;
+    justify-content:center;
+`;
+
+export const Label = styled.Text `
+font-size:12px;
+color:${({focused}) => focused ?'#000' :'#fff'};
+`;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+import Navigation from './Navigation';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+        <Navigation/>
+    </NavigationContainer>
+  );
+}
+
